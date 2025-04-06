beba7b3c75ee4f98e43ab48805b91e85bb56e619
Purrowy <purr@pc.home>
2025-04-06 18:08:37 +0200

    form added

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'running flask app'
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    age = request.form.get('age')
+    return f'Hello {name}, you are {age} years old!'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
